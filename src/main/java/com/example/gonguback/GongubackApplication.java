b8bddb7d1b5bfa04d25de36fabd74adcdef02e27
onemoon <onemoonstudio@gmail.com>
2018-07-01 01:09:47 +0900

    Add : Spring Boot
    version 0.0.1
    
    base

---
+++ src/main/java/com/example/gonguback/GongubackApplication.java
@@ -0,0 +1,12 @@
+package com.example.gonguback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongubackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongubackApplication.class, args);
+	}
+}
