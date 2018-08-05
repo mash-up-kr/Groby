45251db821be0a708dec64515fff53d8beee9515
onemoon <onemoonstudio@gmail.com>
2018-08-05 12:02:08 +0900

    Refactor : User Entity
    Change User Variable to Camel Case

--- src/main/java/com/example/gonggu/domain/user/User.java
+++ src/main/java/com/example/gonggu/domain/user/User.java
@@ -21,7 +21,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long userNum;
     private String userId;
-    private String userPW;
+    private String userPw;
     private String userName;
 
     private String accountNum;
