cfe7d7ea5937916c65516d6c829b5d74c791ceae
Suji Kim <rlatnwl9595@naver.com>
2018-08-06 22:01:42 +0900

    Add : PopularItemList
    - 인기글 보여주는 기능 추가

---
+++ src/main/java/com/example/gonggu/service/item/PopularItemList.java
@@ -0,0 +1,15 @@
+package com.example.gonggu.service.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PopularItemList {
+    private Long itemId;
+    private String title;
+    private String imgPath;
+    private Integer likeNum;        // 좋아요 수
+    private Integer participantNum; // 참여한 사람의 수
+    private Integer amountLimit;    // 공구 수량
+}
