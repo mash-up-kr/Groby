e98d56b255b7856373d4661d73e6227dd4fe1579
soyeon <qkrthdus125@naver.com>
2018-08-07 21:13:01 +0900

    Add : Email Auth
    "close #25"

--- src/main/java/com/example/gonggu/service/user/UserService.java
+++ src/main/java/com/example/gonggu/service/user/UserService.java
@@ -3,7 +3,8 @@ package com.example.gonggu.service.user;
 import com.example.gonggu.domain.user.User;
 import com.example.gonggu.persistence.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.encoding.PasswordEncoder;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +17,8 @@ public class UserService {
     private UserRepository userRepository;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private MailSender sender;
 
     // 해당 유저를 찾아서 리턴해준다.
     // info 의 getUserBy Key를 통해서 메서드를 변경한다.
@@ -78,7 +81,6 @@ public class UserService {
 
     // for Developer not for Service
     public boolean deleteUser(String userId) {
-
         User checkUser = userRepository.findByUserId(userId);
 
         if(checkUser == null){
@@ -87,6 +89,20 @@ public class UserService {
 
         userRepository.delete(checkUser);
         return true;
+    }
+
+
+    public void sendMail(String userEmail) {
+        String key = new TempKey().getKey(4, true);
+
+        SimpleMailMessage msg = new SimpleMailMessage();
+
+        msg.setFrom("groby@gmail.com");
+        msg.setTo(userEmail);
+        msg.setSubject("Groby 인증번호");
+        msg.setText("Groby 에서 인증 메일을 보냅니다. \n \""+key+ "\" 를 앱에서 입력해주세요 \n By Groby");
+
+        this.sender.send(msg);
     }
 
     //info Map<String,Object>
