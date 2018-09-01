6258e11a1a9e6c76952cc236f40af9f065b60ef1
onemoon <onemoonstudio@gmail.com>
2018-09-01 16:47:45 +0900

    Add : NotiService
    in itemService Add Notification proces

--- src/main/java/com/example/gonggu/dto/user/NotiContents.java
+++ src/main/java/com/example/gonggu/dto/user/NotiContents.java
@@ -15,4 +15,35 @@ public class NotiContents {
     private String title;
     private String contents;
 
+    public NotiContents() {
+        // Basic Noti Obj
+    }
+
+    public NotiContents(int tab , User targetUser , Long itemId ,String itemTitle){
+        this.usr = targetUser;
+        this.itemId = itemId;
+        this.itemTitle = itemTitle;
+
+        switch (tab){
+            case 2:
+                this.title = "수량조사";
+                this.contents = "공동구매에 참여하세요!";
+                break;
+            case 3:
+                this.title = "공구진행";
+                this.contents = "공동구매가 진행되고 있습니다.";
+                break;
+            case 4:
+                this.title = "주문 완료";
+                this.contents = "참여하신 공구의 주문이 완료되었습니다.";
+                break;
+            case 5:
+                this.title = "배부공지";
+                this.contents = "배부공지를 확인해주세요.";
+                break;
+            default:
+                break;
+
+        }
+    }
 }
