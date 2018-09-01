1409e743d3f154c39010310cd420a90ff912f513
Suji Kim <rlatnwl9595@naver.com>
2018-09-01 17:29:50 +0900

    Add : Swagger detail information

--- src/main/java/com/example/gonggu/config/SwaggerConfig.java
+++ src/main/java/com/example/gonggu/config/SwaggerConfig.java
@@ -2,8 +2,11 @@ package com.example.gonggu.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,10 +17,23 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("com.example.groby")
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.gonggu.controller"))
                 .paths(PathSelectors.any())
 //                .paths(PathSelectors.regex("\\/(user|category)\\/\\w*"))
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("GROBY")
+                .description("Mash-up 5기 프로젝트")
+                .termsOfServiceUrl("https://github.com/mash-up-kr/Groby")
+                .contact(new Contact("Groby-Backend team", "https://github.com/mash-up-kr/Groby", "mashupdutchmarket@gmail.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .version("1.0")
                 .build();
     }
 }
