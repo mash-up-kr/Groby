068d900c37ab64747554c93d170aaa26af2ca7b1
Suji Kim <rlatnwl9595@naver.com>
2018-09-01 18:31:06 +0900

    Add : user controller swagger description

--- src/main/java/com/example/gonggu/controller/user/UserController.java
+++ src/main/java/com/example/gonggu/controller/user/UserController.java
@@ -5,6 +5,7 @@ import com.example.gonggu.dto.APIResponse;
 import com.example.gonggu.dto.user.*;
 import com.example.gonggu.service.user.NotiService;
 import com.example.gonggu.service.user.UserService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +31,7 @@ public class UserController {
     NotiService notiService;
 
     // 유저 아이디를 통해서 유저 정보 리턴
+    @ApiOperation(value = "apiGetUserByEmail",notes = "유저 아이디(이메일)를 통해서 유저 정보 리턴")
     @GetMapping(value = "/email/{userEmail}")
     public ResponseEntity<APIResponse<UserInfo>> apiGetUserByEmail(
             @PathVariable String userEmail
@@ -51,6 +53,7 @@ public class UserController {
     }
 
     // 유저 넘버 (PK) 를 통해서 유저 정보 리턴
+    @ApiOperation(value = "apiGetUserByUserNum",notes = "유저 넘버 (PK) 를 통해서 유저 정보 리턴")
     @GetMapping(value = "/usernum/{userNum}")
     public ResponseEntity<APIResponse<UserInfo>> apiGetUserByUserNum(
             @PathVariable String userNum
@@ -77,6 +80,7 @@ public class UserController {
 
     //    /user/checkemail?userEail=abc@naver.com
     // return 값 명시해줘야 한다.
+    @ApiOperation(value = "apiCheckEmail",notes = "회원가입시 이메일 인증")
     @GetMapping("/checkemail")
     public ResponseEntity<APIResponse> apiCheckEmail(
             @RequestParam String userEmail
@@ -101,6 +105,7 @@ public class UserController {
     }
 
     // Signup 관련 , 유저를 생성
+    @ApiOperation(value = "apiCreateUser",notes = "유저 회원가입")
     @PostMapping("/")
     public ResponseEntity<APIResponse> apiCreateUser(
         @RequestBody UserSignupJson acceptJson
@@ -117,6 +122,7 @@ public class UserController {
     }
 
     // 유저 로그인
+    @ApiOperation(value = "apiLoginUser",notes = "유저 로그인")
     @PostMapping("/login")
     public ResponseEntity<APIResponse> apiLoginUser(
             @RequestBody UserLoginJson acceptJson
@@ -144,6 +150,7 @@ public class UserController {
     }
 
     // 유저아이디를 통해서 유저의 정보를 수정
+    @ApiOperation(value = "apiChangeUser",notes = "유저 아이디(이메일)를 통해서 유저의 정보를 수정")
     @PatchMapping("/")
     public ResponseEntity<APIResponse> apiChangeUser(
         @RequestBody UserPatchJson acceptJson
@@ -160,6 +167,7 @@ public class UserController {
 
     }
 
+    @ApiOperation(value = "apiChangeUserPw",notes = "비밀번호 변경")
     @PatchMapping("/userPw")
     public ResponseEntity<APIResponse> apiChangeUserPw(
             @RequestBody UserPwJson acceptJson
@@ -177,6 +185,7 @@ public class UserController {
 
     // 유저 아이디를 통해서 유저를 삭제
     // for backend developer
+    @ApiOperation(value = "apiDeleteUserById",notes = "유저 아이디(이메일)를 통해서 유저를 삭제")
     @DeleteMapping("/id/{userEmail}")
     public ResponseEntity<APIResponse> apiDeleteUserById(
         @PathVariable String userId
@@ -200,6 +209,7 @@ public class UserController {
 
     }
 
+    @ApiOperation(value = "apiUserNotifications",notes = "사용자 알림서비스")
     @GetMapping("/{userId}/notifications")
     public ResponseEntity<APIResponse<List<Notification>>> apiUserNotifications(
             @PathVariable String userId
@@ -213,6 +223,7 @@ public class UserController {
         return new ResponseEntity<>(response,status);
     }
 
+    @ApiOperation(value = "apiDelNoti",notes = "사용자 알림서비스 삭제")
     @DeleteMapping("/notification/{notiId}")
     public ResponseEntity<APIResponse> apiDelNoti(
             @PathVariable(name = "notiId") String notiId
