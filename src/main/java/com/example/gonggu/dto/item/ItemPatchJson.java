b1d0e9964e12fac7cc18b81555b7897f2d38acba
Suji Kim <rlatnwl9595@naver.com>
2018-09-30 23:17:57 +0900

    Refactor : Item Api except toggleLike  issue #69

--- src/main/java/com/example/gonggu/dto/item/ItemPatchJson.java
+++ src/main/java/com/example/gonggu/dto/item/ItemPatchJson.java
@@ -5,20 +5,29 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
 @Setter
 @ToString
 public class ItemPatchJson {
+    @NotNull
     private String itemId;
+    @NotNull
     private String writerId;
+    @NotNull
     private Boolean editTab;
+    @NotNull
     private String category;
+    @NotNull
     private String itemTitle;
+    @NotNull
     private Integer targetTab;
 
+    @NotNull
     private String[] imgPathList;
+    @NotNull
     private Integer itemAmountLimit;
 
     private ItemTab1Json tabOne;
